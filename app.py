2d4eafae44c15c70df31b417c68d9f648fe55798
evawallis <ew1466a@student.american.edu>
2024-04-03 16:02:10 +0200

    starting game

--- app.py
+++ app.py
@@ -1,1 +1,42 @@
-print('hello world')
+import random
+
+def main():
+    print('Welcome to the game!')
+    name = input("Enter your name: \n")
+    print(f"Hello {name}!")
+    startGame()
+
+def startGame():
+    print("The game has begun.")
+    game = True
+    while game == True:
+        choice = input("Enter 'rock', 'paper', or 'scissors'.\n")
+        if choice != "rock" and  choice != "paper" and choice != "scissors":
+            print("Try again.")
+            continue
+        else:
+            computerChoice = random.randint(1, 3)
+            if choice == 'rock':
+                choice = 1
+            elif choice == 'paper':
+                choice = 2
+            else:
+                choice = 3
+            if choice == computerChoice:
+                print("Tie!")
+                again = input("Play again?\n")
+                if again == "yes":
+                    continue
+                elif again == "no":
+                    game = False
+                    continue
+                else:
+                    print("Incorrect input. Game will terminate.")
+                    game = False
+                    continue
+            
+            
+
+
+
+main()
